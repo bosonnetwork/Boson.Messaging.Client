4655e0e72b18860db5dee46af57d1fa9c553b1ee
Jingyu <jingyu.niu@gmail.com>
2026-04-14 21:46:29 +0800

    Add test client launcher and sample configuration file

---
+++ src/test/java/io/bosonnetwork/photonmessaging/ConfigurationTests.java
@@ -0,0 +1,127 @@
+package io.bosonnetwork.photonmessaging;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.bosonnetwork.Id;
+import io.bosonnetwork.crypto.Signature;
+import io.bosonnetwork.json.Json;
+import io.bosonnetwork.utils.Base58;
+import io.bosonnetwork.utils.FileUtils;
+import io.bosonnetwork.utils.Hex;
+
+public class ConfigurationTests {
+	private static final Path testDir = Path.of(System.getProperty("java.io.tmpdir"), "boson", "ActiveProxyClient");
+
+	@BeforeAll
+	static void setup() throws Exception {
+		if (Files.exists(testDir))
+			FileUtils.deleteFile(testDir);
+
+		Files.createDirectories(testDir);
+	}
+
+	@AfterAll
+	static void tearDown() throws Exception {
+		if (Files.exists(testDir))
+			FileUtils.deleteFile(testDir);
+	}
+
+	@Test
+	void testBuildConfig() {
+		Configuration config = Configuration.builder()
+				.servicePeerId(Id.random())
+				.serviceEndpoint("mqtt://10.0.0.1:1883")
+				.generateUserKey()
+				.generateDeviceKey()
+				.database("jdbc:sqlite:test.db", 1)
+				.build();
+		assertNotNull(config);
+
+		config = Configuration.builder()
+				.servicePeerId(Id.random())
+				.serviceEndpoint("mqtts://10.0.0.1:8883")
+				.userKey(Base58.encode(Signature.KeyPair.random().privateKey().bytes()))
+				.deviceKey("0x" + Hex.encode(Signature.KeyPair.random().privateKey().bytes()))
+				.databaseUri("postgresql://localhost:5432/test")
+				.databasePoolSize(4)
+				.build();
+		assertNotNull(config);
+
+		assertThrows(IllegalStateException.class, () -> Configuration.builder().build());
+
+		assertThrows(IllegalArgumentException.class, () ->
+				Configuration.builder()
+						.servicePeerId(Id.random())
+						.serviceEndpoint("tcp://10.0.0.1:8883")
+						.generateUserKey()
+						.generateDeviceKey()
+						.database("jdbc:sqlite:test.db", 1)
+						.build()
+		);
+
+		assertThrows(IllegalStateException.class, () ->
+				Configuration.builder()
+						.servicePeerId(Id.random())
+						.serviceEndpoint("mqtts://10.0.0.1:8883")
+						.generateDeviceKey()
+						.database("jdbc:sqlite:test.db", 1)
+						.build()
+		);
+
+		assertThrows(IllegalStateException.class, () ->
+				Configuration.builder()
+						.servicePeerId(Id.random())
+						.serviceEndpoint("mqtts://10.0.0.1:8883")
+						.userKey(Base58.encode(Signature.KeyPair.random().privateKey().bytes()))
+						.deviceKey("0x" + Hex.encode(Signature.KeyPair.random().privateKey().bytes()))
+						.databasePoolSize(4)
+						.build()
+		);
+	}
+
+	@Test
+	void saveAndLoad() throws Exception {
+		Signature.KeyPair userKey = Signature.KeyPair.random();
+		Signature.KeyPair deviceKey = Signature.KeyPair.random();
+
+		Configuration config = Configuration.builder()
+				.service(Id.random(), "mqtts://10.0.0.1:8883")
+				.userKey(userKey)
+				.deviceKey(deviceKey)
+				.database("postgresql://localhost:5432/test", 4)
+				.databaseSchemaName("photon")
+				.build();
+
+		Map<String, Object> configMap = config.toMap();
+		Path testFile = testDir.resolve("config.yaml");
+		Json.yamlMapper().writeValue(testFile.toFile(), configMap);
+
+		System.out.println("User id: " + Id.of(userKey.publicKey().bytes()));
+		System.out.println("Device id: " + Id.of(deviceKey.publicKey().bytes()));
+
+		System.out.println("Configuration:\n-------------");
+		Files.readAllLines(testFile).forEach(System.out::println);
+
+		Map<String, Object> loadedMap = Json.yamlMapper().readValue(testFile.toFile(), Json.mapType());
+		assertEquals(configMap, loadedMap);
+
+		Configuration loaded = Configuration.fromMap(loadedMap);
+		assertEquals(config.getServicePeerId(), loaded.getServicePeerId());
+		assertEquals(config.getServiceEndpoint(), loaded.getServiceEndpoint());
+		assertEquals(config.getUserKey(), loaded.getUserKey());
+		assertEquals(config.getDeviceKey(), loaded.getDeviceKey());
+		assertEquals(config.getDatabaseUri(), loaded.getDatabaseUri());
+		assertEquals(config.getDatabasePoolSize(), loaded.getDatabasePoolSize());
+		assertEquals(config.getDatabaseSchemaName(), loaded.getDatabaseSchemaName());
+	}
+}
---
+++ src/test/java/io/bosonnetwork/photonmessaging/TestClientLauncher.java
@@ -0,0 +1,89 @@
+package io.bosonnetwork.photonmessaging;
+
+import java.io.InputStream;
+import java.net.Inet4Address;
+import java.net.URI;
+import java.util.Map;
+import java.util.Objects;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+import io.bosonnetwork.json.Json;
+import io.bosonnetwork.utils.AddressUtils;
+
+public class TestClientLauncher {
+	private static Vertx vertx;
+	private static MessagingClient client;
+
+	private static Configuration loadConfig() throws Exception{
+		try (InputStream s = TestClientLauncher.class.getClassLoader().getResourceAsStream("testConfig.yaml")) {
+			Map<String, Object> map = Json.yamlMapper().readValue(s, Json.mapType());
+			// fix the server host
+			if (map.containsKey("service")) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> service = (Map<String, Object>) map.get("service");
+				String endpoint = service.getOrDefault("endpoint", "").toString();
+				if (!endpoint.isEmpty()) {
+					String host = Objects.requireNonNull(AddressUtils.getDefaultRouteAddress(Inet4Address.class)).getHostAddress();
+					URI uri = URI.create(endpoint);
+					URI fixed = new URI(uri.getScheme(), uri.getUserInfo(), host, uri.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment());
+					service.put("endpoint", fixed.toString());
+				}
+			}
+			return Configuration.fromMap(map);
+		} catch (Exception e) {
+			System.err.println("Failed to load configuration file: " + e.getMessage());
+			throw e;
+		}
+	}
+
+	public static void main(String[] args) {
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			if (client != null) {
+				System.out.println("Shutting down the active proxy client...");
+				client.stop().thenRun(() -> {
+					System.out.println("Active proxy client stopped.");
+				}).join();
+
+				// Cannot chain vertx.close() to the above future because closing Vert.x will terminate its event loop,
+				// preventing any pending future handlers from executing.
+				System.out.print("Shutting down Vert.x gracefully...");
+				vertx.close().toCompletionStage().toCompletableFuture().join();
+				System.out.println("Done!");
+			}
+		}));
+
+		vertx = Vertx.vertx(new VertxOptions()
+				.setWorkerPoolSize(4)
+				.setEventLoopPoolSize(4)
+				.setPreferNativeTransport(true));
+
+		try {
+			// no node for the client. so the configuration should:
+			// - provide the service peer address (to avoid the dht lookup)
+			// - disable announce peer
+			Configuration config = loadConfig();
+			client = MessagingClient.create(vertx, null, config);
+			client.addConnectionListener(new ConnectionListener() {
+				@Override
+				public void connected() {
+					System.out.println("Connected to the messaging service: " + config.getServicePeerId());
+				}
+
+				@Override
+				public void disconnected() {
+					System.out.println("Disconnected from the messaging service: " + config.getServicePeerId());
+				}
+			});
+
+			System.out.println("Starting the messaging client...");
+			client.start().thenRun(() ->
+				System.out.println("Started the messaging client")
+			).join();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			vertx.close();
+		}
+	}
+}
